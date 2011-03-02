f036b0961fff0f1433852d8862b90c11b1b00e2d
Craig de Stigter <craig.destigter@koordinates.com>
2011-03-03 01:19:40 +1300

    add rule processing with one rule

---
+++ tc/checker.py
@@ -0,0 +1,129 @@
+import logging
+
+from django.template import loader, base, loader_tags, defaulttags
+
+class TemplateChecker(object):
+    registered_rules = []
+    
+    def __init__(self):
+        self.warnings = []
+        self.errors = []
+    
+    def check_template(self, path):
+        """
+        Checks the given template for badness.
+        """
+        try:
+            template = loader.get_template(path)
+        except (base.TemplateSyntaxError, base.TemplateDoesNotExist), e:
+            self.errors.append(e)
+            return
+        
+        rules = [r(self, template) for r in self.registered_rules]
+    
+        # breadth-first search of the template nodes
+        #TODO should probably use deque, since we're doing popleft() a lot?
+        nodes = template.nodelist[:]
+        
+        # set parent nodes, so rules can traverse up the hierarchy if they want
+        for n in nodes:
+            n.parent = None
+        
+        while nodes:
+            node = nodes.pop(0)
+            
+            children = []
+            if isinstance(node, base.TextNode):
+                if not node.s.strip():
+                    # skip further processing for blank text nodes
+                    continue
+            elif getattr(node, 'nodelist', None):
+                children = node.nodelist[:]
+                for child in children:
+                    child.parent = node
+            
+            valid = True
+            for rule in rules:
+                if rule.visit_node(node) is False:
+                    valid = False
+                    rule.log(node)
+            if valid:
+                nodes.extend(children)
+    
+    def _log(self, level, node, message):
+        # TODO get line number of node in template somehow
+        logging.log(level, message)
+    
+    def info(self, node, message):
+        self._log(logging.INFO, node, message)
+    
+    def warn(self, node, message):
+        self.warnings.append(message)
+        self._log(logging.WARN, node, message)
+    
+    def error(self, node, message):
+        self.errors.append(message)
+        self._log(logging.ERROR, node, message)
+
+
+### RULES - base classes
+
+class RuleMeta(type):
+    """
+    Automatically register rule classes with TemplateChecker
+    """
+    def __new__(meta, className, bases, classDict):
+        cls = type.__new__(meta, className, bases, classDict)
+        try:
+            Rule
+        except NameError:
+            pass
+        else:
+            TemplateChecker.registered_rules.append(cls)
+        return cls
+
+class Rule(object):
+    """
+    Determines when a node is legal and when it isn't.
+    Nodes are visited in a breadth-first fashion.
+    """
+    __metaclass__ = RuleMeta
+    def __init__(self, checker, template):
+        """
+        Create a Rule for the given checker and template.
+        """
+        self._info = {}
+        self.checker = checker
+        self.template = template
+    
+    def visit_node(self, node):
+        """
+        Returns whether a node is valid in the current context.
+        If False, the node's children will not be processed.
+        """
+        return None
+    
+    def log(self, node):
+        """
+        Must be implemented to log an error or warning for the node.
+        This is only called if visit_node() returns False.
+        """
+        raise NotImplementedError
+
+
+### RULES - actual rules
+
+class TextOutsideBlocksInExtended(Rule):
+    """
+    No point having text nodes outside of blocks in extended templates.
+    """
+    def visit_node(self, node):
+        if isinstance(node, loader_tags.ExtendsNode):
+            self._info['extends_node'] = node
+        elif self._info.get('extends_node'):
+            if not isinstance(node, (loader_tags.BlockNode, defaulttags.LoadNode, defaulttags.CommentNode)):
+                if node.parent == self._info['extends_node']:
+                    return False
+    
+    def log(self, node):
+        self.checker.warn(node, 'Non-empty text node outside of blocks in extended template')
--- tests/myapp/tests.py
+++ tests/myapp/tests.py
@@ -1,9 +1,29 @@
 from django.test import TestCase
 
 from tc.loading import gen_all_templates
+from tc.checker import TemplateChecker
 
 class TestLoading(TestCase):
     def test_app_directories_loader(self):
         templates = [tupl[0] for tupl in gen_all_templates()]
         self.assertTrue('base1.html' in templates)
         self.assertTrue('extends1.html' in templates)
+
+
+class TemplatesTestCase(TestCase):
+    def setUp(self):
+        self.checker = TemplateChecker()
+
+class TestBaseBlocks(TemplatesTestCase):
+    def test_happy_templates(self):
+        for t in ['base1.html', 'extends1.html']:
+            self.checker.check_template(t)
+        
+        self.assertEqual(len(self.checker.warnings), 0)
+        self.assertEqual(len(self.checker.errors), 0)
+
+    def test_extends_template_extra_bits(self):
+        self.checker.check_template('extends1-badblocks.html')
+        
+        self.assertEqual(len(self.checker.warnings), 1)
+        self.assertEqual(len(self.checker.errors), 0)
