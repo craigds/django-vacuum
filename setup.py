06edf2fc0fdbbc7225962c2939a5d166beebcbd9
Craig de Stigter <craig.destigter@koordinates.com>
2011-03-05 16:42:42 +1300

    add setup.py and VERSION

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import vacuum
+
+setup(name='django-vacuum',
+    version=vacuum.VERSION,
+    description='Sucks the lint from your Django templates',
+    author='Craig de Stigter',
+    author_email='craig.ds@gmail.com',
+    url='https://github.com/craigds/django-vacuum',
+    packages=[
+        'vacuum',
+        'vacuum.management',
+        'vacuum.management.commands',
+    ],
+)
