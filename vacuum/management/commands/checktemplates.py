c267fa0f2b1638e4f8654464bc649a845406ee3b
Craig de Stigter <craig.destigter@koordinates.com>
2011-03-07 10:30:30 +1300

    er, make it actually do something when run apart from teh test suite ;)

--- vacuum/management/commands/checktemplates.py
+++ vacuum/management/commands/checktemplates.py
@@ -2,6 +2,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 
+from vacuum.checker import TemplateChecker
 from vacuum.loading import gen_all_templates
 
 verbosity_map = {
@@ -19,6 +20,7 @@ class Command(BaseCommand):
         log_level = verbosity_map[int(options['verbosity'])]
         logging.basicConfig(level=log_level)
         
+        checker = TemplateChecker()
+        
         for rel_path, abs_path in gen_all_templates():
-            pass
-
+            checker.check_template(rel_path)
