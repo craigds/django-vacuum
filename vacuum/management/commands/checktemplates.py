85b2300e2edf0dad65ba63b4394e8285e44e8bf4
Chris Adams <chris@improbable.org>
2011-03-08 09:12:18 -0500

    Minor pep8 tweaks

--- vacuum/management/commands/checktemplates.py
+++ vacuum/management/commands/checktemplates.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 from vacuum.checker import TemplateChecker
 from vacuum.loading import gen_all_templates
 
+
 verbosity_map = {
     0: logging.ERROR,
     1: logging.WARNING,
@@ -12,16 +13,17 @@ verbosity_map = {
     3: logging.DEBUG,
 }
 
+
 class Command(BaseCommand):
     args = '[--verbosity]'
     help = 'Checks templates for badness'
-    
+
     def handle(self, *args, **options):
         log_level = verbosity_map[int(options['verbosity'])]
         logging.basicConfig(format="%(levelname)s: %(message)s",
                             level=log_level)
 
         checker = TemplateChecker()
-        
+
         for rel_path, abs_path in gen_all_templates():
             checker.check_template(rel_path)
